dbcaf3eff6c3c6e28a22732679085edeb8fdccd6
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2023-04-08 21:35:49 +0700

    init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: flex;
+`;
+
+const StyledP = styled.p`
+  font-size: 50px;
+`;
+
+function App() {
+  return (
+    <StyledDiv>
+      <p>eiei</p>
+      <StyledP>eiei</StyledP>
+    </StyledDiv>
+  );
+}
+
+export default App;
