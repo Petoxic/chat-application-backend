451b3e58977f9920f336fbac3805b41cca93ec28
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2023-04-12 22:10:41 +0700

    create message when joining room

---
+++ frontend/src/components/chatRoom/JoiningMessage.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+import theme from "../../utils/theme";
+
+const JoiningMessage = ({message}) => {
+  return (
+    <ContentContainer>
+      <MessageWrapper>{message}</MessageWrapper>
+    </ContentContainer>
+  );
+};
+
+export default JoiningMessage;
+
+const ContentContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const MessageWrapper = styled.p`
+  width: 70%;
+  background-color: ${theme.color.secondary};
+  border: 1px solid ${theme.color.primary};
+  border-radius: 5px;
+  text-align: center;
+`;
