5dbfe047aa6148b090c18577e2778aa2a49b9844
Bugguga <nat14660@hotmail.com>
2023-04-26 09:59:06 +0700

    Update socket.js

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
-const socket = io.connect("http://localhost:3001");
-// const socket = io.connect("https://chat-server-network.herokuapp.com/");
+// const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://chat-server-network.herokuapp.com/");
 
 export const joinChat = (username) => {
   socket.emit("joinChat", username);
@@ -23,8 +23,8 @@ export const sendDirectMessage = (username, talker, message) => {
 };
 
 export const searchRoom = (input, username) => {
-  socket.emit("searchRooms", {input, username});
-}
+  socket.emit("searchRooms", { input, username });
+};
 
 export const getSocket = () => {
   return socket;
